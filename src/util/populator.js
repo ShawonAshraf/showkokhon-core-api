1857e9a9dbfa997ed0e7f7836140826ecaeb7fa5
Shawon Ashraf <shawon13@live.com>
2019-06-16 18:24:34 +0600

    db populator

---
+++ src/util/populator.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const Movie = require('../models/movie');
+
+const populateDb = async (force) => {
+  const currentTime = new Date();
+
+  if (force || ((currentTime.getHours() === 23 && currentTime.getMinutes() === 0)
+    || (currentTime.getHours() === 17 && currentTime.getMinutes() === 0)
+    || (currentTime.getHours() === 11 && currentTime.getMinutes() === 0))) {
+    // fetch from scraper and push
+    try {
+      // clear db
+      await Movie.remove({});
+
+      const response = await axios.get(process.env.API_END);
+      const schedules = response.data.movies;
+
+      console.log('populating db');
+      schedules.forEach((element) => {
+        const entry = new Movie(element);
+        entry.save().then(movie => movie, err => err);
+      });
+
+      return { status: 'Scraped and Updated Database' };
+    } catch (e) {
+      return { error: e };
+    }
+  } else {
+    return { status: 'Database already updated.' };
+  }
+};
+
+module.exports = {
+  populateDb,
+};
