8c6497a0fa72df1e112030ca86efd38426301584
Shawon Ashraf <shawon13@live.com>
2019-06-16 18:54:23 +0600

    /all /:id

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -1,14 +1,26 @@
 const express = require('express');
 
+const {
+  fetchAllSchedule,
+  fetchScheduleByCinemaId,
+} = require('./../util/fetcher');
+
 // init router
 const scheduleRouter = express.Router();
 
 // get all schedule
-scheduleRouter.get('/all', (req, res) => {
-  // send all schedule
-  res.status(200).send({
+scheduleRouter.get('/all', async (req, res) => {
+  const schedules = await fetchAllSchedule();
+  res.send(schedules);
+});
 
-  });
+// get schedule by cinema id
+// 0 for star cineplex
+// 1 for blockbuster
+scheduleRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const schedules = await fetchScheduleByCinemaId(id);
+  res.send(schedules);
 });
 
 module.exports = scheduleRouter;
