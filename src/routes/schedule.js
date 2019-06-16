fc790f1b92469f19762391d88f8366069d7f5350
Shawon Ashraf <shawon13@live.com>
2019-06-17 01:09:03 +0600

    need mongodb aggregation

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const {
   fetchAllSchedule,
-  fetchScheduleByCinemaId,
 } = require('./../util/fetcher');
 
 // init router
@@ -11,15 +10,6 @@ const scheduleRouter = express.Router();
 // get all schedule
 scheduleRouter.get('/all', async (req, res) => {
   const schedules = await fetchAllSchedule();
-  res.send(schedules);
-});
-
-// get schedule by cinema id
-// 0 for star cineplex
-// 1 for blockbuster
-scheduleRouter.get('/cinema/:id', async (req, res) => {
-  const { id } = req.params;
-  const schedules = await fetchScheduleByCinemaId(id);
   res.send(schedules);
 });
 
--- src/util/fetcher.js
+++ src/util/fetcher.js
@@ -7,35 +7,11 @@ const locations = [
   'Shimanto Shambhar, Dhanmondi 2',
 ];
 
-// const codeMap = {
-//   ss: 'Shimanto Shambhar, Dhanmondi 2',
-//   cp: 'Bashundhara Shopping Mall, Panthapath',
-// };
-
 const fetchAllSchedule = async () => {
   const schedules = await Movie.find({});
   return schedules;
 };
 
-const fetchScheduleByCinemaId = async (id) => {
-  const schedules = [];
-
-  const schedulesIds = await Movie.find({}, { cinemaId: id });
-
-  for (let i = 0; i < schedulesIds.length; i += 1) {
-    const objId = new ObjectId(schedulesIds[i]._id);
-
-    const schedule = await Movie.find({ _id: objId });
-    schedules.push(schedule[0]);
-  }
-
-  return schedules;
-};
-// const fetchStarCineplexScheduleByLocation = async code =>
-//   // TODO: to be done after api fix
-//   ({});
-
 module.exports = {
   fetchAllSchedule,
-  fetchScheduleByCinemaId,
 };
