8a8fdae5e82d240bc452b18ee003ee2e2e71b471
Shawon Ashraf <shawon13@live.com>
2019-06-16 15:54:50 +0600

    middlwares

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
-let express = require('express');
-let morgan = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
-let app = express();
+const app = express();
+
+// add middlewares
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+// cors
+app.use(cors());
+
+// root route
+app.get('/core/v1/', (req, res) => {
+  res.status(200).send({
+    msg: 'Showkokohon-Core-API',
+    sentAt: Date(),
+  });
+});
 
 app.listen(port, () => console.log(`server runnung @ ${port}`));
