a767668938ebf171479d0f4c4f0d98e7ae917175
Shawon Ashraf <shawon13@live.com>
2019-06-18 14:56:10 +0600

    tests covered

---
+++ tests/fetcher.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest');
+const expect = require('expect');
+
+const {
+  locations,
+  fetchAllSchedule,
+  fetchScheduleByCinemaId,
+  fetchStarCineplexScheduleByLocationId,
+} = require('../src/util/fetcher');
+
+const {
+  populate,
+  count
+} = require('./seed');
+
+
+describe('fetcher.js', () => {
+  it('should fetch all schedule', (done) => {
+    fetchAllSchedule()
+      .then((docs) => {
+        expect(docs.length).toEqual(count());
+        done();
+      })
+      .catch(e => done(e));
+  });
+
+  it('should return schedule by cinemaId', (done) => {
+    fetchScheduleByCinemaId(0)
+      .then((docs) => {
+        const { schedule } = docs[0];
+        const { cinemaId } = schedule[0].playingAt[0];
+
+        expect(cinemaId).toEqual(0);
+        done();
+      })
+      .catch(e => done(e));
+  });
+
+  it('should return star cineplex schedule by location id', (done) => {
+    fetchStarCineplexScheduleByLocationId(1)
+      .then((docs) => {
+        const { schedule } = docs[0];
+        const { locationName } = schedule[0].playingAt[0];
+
+        expect(locationName).toBe(locations[1]);
+        done();
+      })
+      .catch(e => done(e));
+  });
+});
