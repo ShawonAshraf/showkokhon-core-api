1ac2deb5edf1c4e614a52540da3d16d158956fda
Shawon Ashraf <shawon13@live.com>
2019-06-18 14:02:26 +0600

    no cron job in test env

--- src/util/cronjob.js
+++ src/util/cronjob.js
@@ -2,13 +2,16 @@ const cron = require('node-cron');
 
 const { populateDb } = require('./populator');
 
-cron.schedule('0 */4 * * *', async () => {
-  console.log(`CronJob @ ${new Date()}`);
 
-  try {
-    const res = await populateDb();
-    console.log(res);
-  } catch (e) {
-    console.log(e);
-  }
-});
+if (process.env.NODE_ENV !== 'test') {
+  cron.schedule('0 */4 * * *', async () => {
+    console.log(`CronJob @ ${new Date()}`);
+
+    try {
+      const res = await populateDb();
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
