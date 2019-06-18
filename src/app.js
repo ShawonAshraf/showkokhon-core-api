f20840610a2789aa4fbb6ac9f4710a9837b38836
Shawon Ashraf <shawon13@live.com>
2019-06-18 14:06:51 +0600

    removed populator route

--- src/app.js
+++ src/app.js
@@ -43,17 +43,6 @@ app.get('/core/v1/', (req, res) => {
   });
 });
 
-// populator
-// fetches from scraper and populates db beforehand
-app.get('/core/v1/populate', async (req, res) => {
-  try {
-    const status = await populateDb();
-    res.send(status);
-  } catch (e) {
-    res.send({ error: e });
-  }
-});
-
 app.listen(port, async () => {
   console.log(`server running @ ${port}`);
 });
