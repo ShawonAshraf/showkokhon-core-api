0fab9e5781e2bfbc8156f2f66bb6ecd7bafc5d07
Shawon Ashraf <shawon13@live.com>
2019-06-18 21:24:53 +0600

    cronjob updated

--- src/util/cronjob.js
+++ src/util/cronjob.js
@@ -4,7 +4,7 @@ const { populateDb } = require('./populator');
 
 
 if (process.env.NODE_ENV !== 'test') {
-  cron.schedule('0 */4 * * *', async () => {
+  cron.schedule('0 */2 * * *', async () => {
     console.log(`CronJob @ ${new Date()}`);
 
     try {
