12b51e9d89b6227a8f894174a91f265e7557573a
Shawon Ashraf <shawon13@live.com>
2019-06-21 11:33:26 +0600

    run cronjob every 30 minutes

--- src/util/cronjob.js
+++ src/util/cronjob.js
@@ -5,7 +5,7 @@ const { populateDb } = require('./populator');
 // production usage only
 
 if (process.env.NODE_ENV !== 'test' || process.env.NODE_ENV !== 'development') {
-  cron.schedule('0 */4 * * *', async () => {
+  cron.schedule('*/30 * * * *', async () => {
     console.log(`CronJob @ ${new Date()}`);
 
     try {
