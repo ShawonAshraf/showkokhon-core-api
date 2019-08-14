8e47362f87af60075e360fb16e10f1fc43d8feb6
Shawon Ashraf <shawon13@live.com>
2019-08-14 14:08:20 +0600

    db won't be wiped out in case scraper fails

--- src/util/populator.js
+++ src/util/populator.js
@@ -6,20 +6,34 @@ const populateDb = async () => {
   const currentTime = new Date();
   // fetch from scraper and push
   try {
-    // clear db
-    await Movie.remove({});
-
     const response = await axios.get(process.env.API_END);
-    const schedules = response.data.movies;
 
-    schedules.forEach((element) => {
-      const entry = new Movie(element);
-      entry.save().then(movie => movie, err => err);
-    });
+    if (response.status === 200) {
+      // clear db
+      await Movie.remove({});
 
-    return { status: 'Scraped and Updated Database', sent_at: currentTime };
+      const schedules = response.data.movies;
+
+      schedules.forEach((element) => {
+        const entry = new Movie(element);
+        entry.save().then(movie => movie, err => err);
+      });
+
+      return {
+        status: 'Scraped and Updated Database',
+        sent_at: currentTime,
+      };
+    }
+
+    // in case scraper fails!
+    return {
+      status: 'Scraper Service Error',
+      sent_at: currentTime,
+    };
   } catch (e) {
-    return { error: e };
+    return {
+      error: e,
+    };
   }
 };
 
