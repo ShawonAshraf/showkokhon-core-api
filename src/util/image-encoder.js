6a4061c9cc017fe3c5e605e5a91998eb5a112266
Shawon Ashraf <shawon13@live.com>
2019-09-19 22:05:46 +0600

    comments

--- src/util/image-encoder.js
+++ src/util/image-encoder.js
@@ -1,5 +1,6 @@
 const { requestBase64 } = require('base64-img');
 
+// wrap the callback in a promise
 const base64EncodingFromURL = url => new Promise((resolve, reject) => {
   requestBase64(url, (err, res, body) => {
     if (err) {
@@ -9,6 +10,7 @@ const base64EncodingFromURL = url => new Promise((resolve, reject) => {
   });
 });
 
+// just return the base64 data
 const encodeImageToBase64 = async (url) => {
   const data = await base64EncodingFromURL(url);
   return data;
