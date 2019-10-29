b763e7c0b36d642592b6028cf062804baa521907
Shawon Ashraf <shawon13@live.com>
2019-10-29 20:12:39 +0100

    added useUnifiedTopology: true to db connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
